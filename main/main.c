d958f6a5106cfe3b378ea3d09150446e0f16d56b
quackers19 <liam.j.tighe@gmail.com>
2026-07-02 22:02:01 +1000

    initial library test

--- main/main.c
+++ main/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include "esp_log.h"
+
+#include <pb_encode.h>
+#include "gtfs-realtime.pb.h"
+
+static const char *TAG = "example";
 
 void app_main(void)
 {
-
+    ESP_LOGI(TAG, "Test");
 }
